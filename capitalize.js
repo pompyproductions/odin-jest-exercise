63a4a30379f33359276e991730a97bf0ee803a93
Eren Gazioglu <pompyproduction@gmail.com>
2023-06-28 14:30:03 +0200

    write capitalize function

--- capitalize.js
+++ capitalize.js
@@ -1,4 +1,19 @@
+const FOREIGN_LETTERS = {
+  ş: "Ş",
+  ı: "I"
+}
+
 function capitalize(string) {
+  const firstLetterCode = string.charCodeAt(0);
+  if (
+    (firstLetterCode > 97 && firstLetterCode <= 122) ||
+    (firstLetterCode > 224 && firstLetterCode <= 254)
+    ) {
+    return `${String.fromCharCode(firstLetterCode - 32)}${string.slice(1)}`
+  }
+  if (Object.hasOwn(FOREIGN_LETTERS, string.charAt(0))) {
+    return `${FOREIGN_LETTERS[string.charAt(0)]}${string.slice(1)}`
+  }
   return string
 }
 
--- capitalize.test.js
+++ capitalize.test.js
@@ -1,5 +1,24 @@
 const capitalize = require("./capitalize");
 
-test("normal string", () => {
+test("basic unicode string", () => {
   expect(capitalize("hello world!")).toBe("Hello world!");
+  expect(capitalize("what's up world!")).toBe("What's up world!");
+});
+
+test("start with non-letter", () => {
+  expect(capitalize(" i start with a space")).toBe(" i start with a space");
+  expect(capitalize("0 letters in the beginning")).toBe("0 letters in the beginning");
+});
+
+test("start with capitalized letter", () => {
+  expect(capitalize("I'm already good")).toBe("I'm already good");
+});
+
+test("first letter accent", () => {
+  expect(capitalize("è problematico")).toBe("È problematico");
+  expect(capitalize("çipura")).toBe("Çipura");
+})
+
+test("first letter foreign", () => {
+  expect(capitalize("şarap")).toBe("Şarap");
 })
